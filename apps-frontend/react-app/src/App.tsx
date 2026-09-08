89aba1ba75e8b4a9886aa3041ea9c9a8c5baf555
Jakob Topholt Jensen <jakob.topholt99@gmail.com>
2026-09-08 09:43:04 +0200

    initial commit

---
+++ apps-frontend/react-app/src/App.tsx
@@ -0,0 +1,102 @@
+import { useState } from 'react'
+//import heroImg from './assets/hero.png'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from './assets/vite.svg'
+import JTJBlack from '../../../shared/JTJBlack.png'
+import LinkedinIcon from '../../../shared/SoMeIcons/LinkedIn_icon.svg'
+import GitHubCatWhite from '../../../shared/SoMeIcons/GitHub_Invertocat_White.svg'
+import PhoneIcon from '../../../shared/phone-call-svgrepo-com.svg'
+import EmailIcon from '../../../shared/email-1-svgrepo-com.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={JTJBlack} className="base" width="120" height="179" alt="MyWIPLogo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        
+        <div id="social">
+          <h2>Connect with me</h2>
+          <p>I try not to bite</p>
+          <ul>
+            <li>
+              <a href="https://github.com/JakobTopholt" target="_blank">
+                <img
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                  src={GitHubCatWhite}
+                  alt="GitHub cat Icon"
+                />
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/jakob-topholt-jensen-0a3780231/" target="_blank">
+                <img
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                  src={LinkedinIcon}
+                  alt="LinkedIn Icon"
+                />
+                Linkedin
+              </a>
+            </li>
+            <li>
+              <a href="tel:+45275340659">
+                <img
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                  src={PhoneIcon}
+                  alt="LinkedIn Icon"
+                />
+                +45 27540659
+                </a>
+            </li>
+                        <li>
+              <a href="mailto:jakob.topholt99@gmail.com">
+                <img
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                  src={EmailIcon}
+                  alt="LinkedIn Icon"
+                />
+                jakob.topholt99@gmail.com
+                </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
